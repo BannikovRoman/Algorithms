4ff3a629d865767debe7b3f261e3532da41d5ab6
rbannikov <O3R9B318464_Sar>
2018-10-22 12:47:28 +0400

    add Fibonacci number

--- src/javaAlgorithms/FibonacciNumber/FibonacciNum.java
+++ src/javaAlgorithms/FibonacciNumber/FibonacciNum.java
@@ -16,7 +16,7 @@ public class FibonacciNum {
         int[] f = new int[n];
         f[0] = 0;
         f[1] = 1;
-        for (int i = 2; i < n; ++i){
+        for (int i = 2; i < n; i++){
             f[i] = f[i - 1] + f[i - 2];
         }
 
